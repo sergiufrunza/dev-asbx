082ca20fc7f2f448e6dd63206da797eb8db18036
sergiufrunza <sergiu.frunza120@gmail.com>
2023-06-01 18:34:44 +0300

    test3

--- web/CalculatorAPP/settings.py
+++ web/CalculatorAPP/settings.py
@@ -16,7 +16,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DEBUG=config("DEBUG")
+DEBUG=config('DEBUG', cast=bool)
 SECRET_KEY = config("SECRET_KEY")
 ALLOWED_HOSTS = ["dev-asbx.us-east-2.elasticbeanstalk.com"]
 
